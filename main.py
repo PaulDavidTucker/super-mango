3912ce24b31949d739c8973e2aa074d80ba431a7
atuck10 <119117977+atuck10@users.noreply.github.com>
2023-05-13 14:01:16 +0100

    yeaahhhhh budddyyyyy

--- main.py
+++ main.py
@@ -2,24 +2,54 @@ import pygame
 
 #A file written for this project, stored in local directory I think.
 import sys
+sys.path.insert(1, r'C:/Users/adamt/OneDrive/Documents/programming/super-mango/modules') 
+
+#Screensize used to determine screenSize dynamically.
+import screenSize
+#Log used to log messages. If attempting to debug just use a logger
+import log as logger
+
 pygame.init()
 
-screen = pygame.display.set_mode((1000,800))
+# Create the window - GameSize here
+screen = pygame.display.set_mode((screenSize.SCREEN_WIDTH, screenSize.SCREEN_HEIGHT))
+
+#Log info
+logger.log.info(f"Screen Size set at: {screenSize.SCREEN_WIDTH} by {screenSize.SCREEN_HEIGHT}")
+
 # Load the sprite image
 sprite_image = pygame.image.load("./assets/Bird_1.png")
 
-# Create the sprite
+# Create the spritegit 
 sprite = pygame.sprite.Sprite()
 sprite.image = sprite_image
 sprite.rect = sprite.image.get_rect()
 sprite.rect.center = screen.get_rect().center
 
-def startmenu()
-    logo = pygame.image.load("C:\Users\adamt\OneDrive\Documents\programming\super-mango\assets\Logo.png")
-    screen.blit(logo, (500,500))
+#variables for height and width for simplification
+height = int(screenSize.SCREEN_HEIGHT)
+width = int(screenSize.SCREEN_WIDTH)
+MidpointWidth = (screenSize.SCREEN_WIDTH / 2)
+MidpointHeight = (screenSize.SCREEN_HEIGHT / 8)
+
+#loading new images and resizing them to be the correct fit
+logo = pygame.image.load("C:/Users/adamt/OneDrive/Documents/programming/super-mango/assets/Logo.png")
+logo = pygame.transform.scale(logo,(300,200))
+skybg0 = pygame.image.load("C:/Users/adamt/OneDrive/Documents/programming/super-mango/assets/Sky_Background_0.png")
+skybg0 = pygame.transform.scale(skybg0, (width, height))
+skybg1 = pygame.image.load("C:/Users/adamt/OneDrive/Documents/programming/super-mango/assets/Sky_Background_1.png")
+skybg1 = pygame.transform.scale(skybg1, (width, height))
+skybg2 = pygame.image.load("C:/Users/adamt/OneDrive/Documents/programming/super-mango/assets/Sky_Background_2.png")
+skybg2 = pygame.transform.scale(skybg2, (width, height))
+
+# function for the start menu
+def startmenu():
+    screen.blit(skybg0,(0,0))
+    screen.blit(skybg1,(0,0))
+    screen.blit(skybg2,(0,0))
+    screen.blit(logo,(MidpointWidth - 150 ,MidpointHeight))
 
 
-startmenu()
 # Game loop
 running = True
 while running:
@@ -27,12 +57,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    startmenu()
     # Draw the sprite and update the display
-    screen.fill((255, 255, 255))
-    screen.blit(sprite.image, sprite.rect)
+    
     pygame.display.update()
-
 
 # Clean up
 pygame.quit()
